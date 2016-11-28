2d1379a1a3c85c8fd89273359fa0e261abd17784
Marcin-Kurpiewski <kalinka123A>
2016-11-28 11:50:13 +0100

    mobile menu toggle class. change menu item.

---
+++ js/main-script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function(){
+    $(".mobile-menu").on("click", function(){
+        $(".menu").toggleClass("mobile-menu-close");
+
+    });
+    { $(".button-send").on("click", function(){
+        $(".sign-nav-btn").remove();
+        $(".game-nav-btn").append().css('display','block');
+        console.log('click');
+
+    })}
+});
